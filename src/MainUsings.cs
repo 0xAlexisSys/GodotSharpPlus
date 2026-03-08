eb27463419e4f028a3777a5b9dcebefc2dc6daa0
Alexis Brooks <91137432+0xAlexisSys@users.noreply.github.com>
2026-03-08 20:10:15 +0600

    Add type aliases for Int128, UInt128, and Half

--- src/MainUsings.cs
+++ src/MainUsings.cs
@@ -5,11 +5,20 @@ global using s8 = sbyte;
 global using s16 = short;
 global using s32 = int;
 global using s64 = long;
+#if NET7_0_OR_GREATER
+global using s128 = System.Int128;
+#endif
 global using u8 = byte;
 global using u16 = ushort;
 global using u32 = uint;
 global using u64 = ulong;
+#if NET7_0_OR_GREATER
+global using u128 = System.UInt128;
+#endif
 global using iLG = System.Numerics.BigInteger;
+#if NET5_0_OR_GREATER
+global using f16 = System.Half;
+#endif
 global using f32 = float;
 global using f64 = double;
 global using f128 = decimal;
